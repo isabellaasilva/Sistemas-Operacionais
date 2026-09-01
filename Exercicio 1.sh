c0c438aa32e043561b20c644b88127dfd8ed63e0
Isabella Silva <bella.silva2003@gmail.com>
2026-09-01 12:12:44 -0300

    Update Exercicio 1.sh

--- Exercicio 1.sh
+++ Exercicio 1.sh
@@ -10,3 +10,20 @@ echo "5)Outro"
 read -p "Digite a operação escolhida: " x 
 case $x in 
 1) echo "Soma" 
+soma=$(expr $n1 + $n2) 
+echo "O Resultado da soma é '$soma'" 
+;; 
+2)echo "Subtração" 
+soma=$(expr $n1 - $n2) 
+echo "O Resultado da subtração é '$soma'"
+;;
+3)echo "Multiplicação" 
+soma=$(expr $n1 \* $n2) 
+echo "O Resultado da multiplicação é '$soma'" 
+;;
+4)echo "Divisão" 
+soma=$(expr $n1 / $n2) 
+echo "O Resultado da divisão é '$soma'" 
+;; 
+*)echo "Outro" 
+esac 
